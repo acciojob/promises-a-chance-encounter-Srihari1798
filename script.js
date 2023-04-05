0caa1daef715859e1c1a6c61116d0a308d0d7a9e
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-04-05 22:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const promises = [];
+
+for (let i = 1; i <= 5; i++) {
+  const promise = new Promise((resolve, reject) => {
+    const random = Math.random();
+    if (random < 0.5) {
+      reject(`Promise ${i} rejected with error`);
+    } else {
+      resolve(`Promise ${i} resolved with ${Math.floor(Math.random() * 10) + 1}`);
+    }
+  });
+  promises.push(promise);
+}
+
+Promise.all(promises)
+  .then((results) => {
+    results.forEach((result) => {
+      const p = document.createElement('p');
+      p.innerText = result;
+      document.getElementById('output').appendChild(p);
+    });
+  })
+  .catch((error) => {
+    const p = document.createElement('p');
+    p.innerText = error;
+    document.getElementById('output').appendChild(p);
+  });
