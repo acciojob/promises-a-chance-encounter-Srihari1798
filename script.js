da564c9502a549c49cc07e154c03c0a2fe7e6be7
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-04-05 22:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-//your JS code here. If required.
-const promises = [];
-
+// Generate five <div> elements with IDs
 for (let i = 1; i <= 5; i++) {
-  const promise = new Promise((resolve, reject) => {
-    const random = Math.random();
-    if (random < 0.5) {
-      reject(`Promise ${i} rejected with error`);
-    } else {
-      resolve(`Promise ${i} resolved with ${Math.floor(Math.random() * 10) + 1}`);
-    }
-  });
-  promises.push(promise);
+  const div = document.createElement('div');
+  div.id = `div-${i}`;
+  document.body.appendChild(div);
 }
-
-Promise.all(promises)
-  .then((results) => {
-    results.forEach((result) => {
-      const p = document.createElement('p');
-      p.innerText = result;
-      document.getElementById('output').appendChild(p);
-    });
-  })
-  .catch((error) => {
-    const p = document.createElement('p');
-    p.innerText = error;
-    document.getElementById('output').appendChild(p);
-  });
+// Check that there are five <div> elements with IDs
+for (let i = 1; i <= 5; i++) {
+  cy.get(`#div-${i}`).should('exist');
+}
